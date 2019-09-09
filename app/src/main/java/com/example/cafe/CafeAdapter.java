576a7a1b8e4319d257090395843e9f655d99a69c
GwanYeop <kukeke09@gmail.com>
2019-09-09 10:06:59 +0900

    Ver 0.3 - 헤더와 푸터 추가 Ver 0.2 - 검색 기능추가 Cafe RecyclerView Ver 0.1 - 퍼스트 커밋

--- app/src/main/java/com/example/cafe/CafeAdapter.java
+++ app/src/main/java/com/example/cafe/CafeAdapter.java
@@ -1,5 +1,6 @@
 package com.example.cafe;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,16 +8,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 
 
-public class CafeAdapter extends RecyclerView.Adapter<CafeAdapter.ItemViewHolder>{
-
+public class CafeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+    private final int TYPE_HEADER = 0;
+    private final int TYPE_ITEM = 1;
+    private final int TYPE_FOOTER = 2;
     private ArrayList<Data> listData;
-
+    @Nullable
     // 리스너 객체 참조를 저장하는 변수입니다.
     private OnItemClickListener CafeListener = null;
     //커스텀 온클릭 리스너입니다.
@@ -25,36 +29,66 @@ public class CafeAdapter extends RecyclerView.Adapter<CafeAdapter.ItemViewHolder
     public void setOnItemClickListener(OnItemClickListener listener){this.CafeListener = listener;}
 
     // adapter에 들어갈 list 입니다.//
-    public CafeAdapter(ArrayList<Data> ListData){
+    protected CafeAdapter(ArrayList<Data> ListData){
         listData = ListData;
-        setHasStableIds(true);
     }
     public long getItemId(int position){
         return position;
     }
     @NonNull
     @Override
-    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        // LayoutInflater를 이용하여 전 단계에서 만들었던 item.xml을 inflate 시킵니다.
-        // return 인자는 ViewHolder 입니다.
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_cafe_item, parent, false);
-        return new ItemViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        RecyclerView.ViewHolder holder;
+        View view;
+        if (viewType == TYPE_HEADER) {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.header, parent, false);
+            holder = new FooterViewHolder(view);
+        } else if (viewType == TYPE_FOOTER) {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.footer, parent, false);
+            holder = new FooterViewHolder(view);
+        } else {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_cafe_item, parent, false);
+            holder = new ItemViewHolder(view);
+        }
+        return holder;
     }
-
     @Override
-    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
-        Data abc = listData.get(position);
+    public int getItemViewType(int position){
+       if(position == 0){
+           return TYPE_HEADER;
+       }
+        else if(position == listData.size() + 1){
+            return TYPE_FOOTER;
+        }else
+            return TYPE_ITEM;
+    }
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if(holder instanceof HeaderViewHolder){
+            HeaderViewHolder headerViewHolder = (HeaderViewHolder) holder;
+
+        }else if(holder instanceof FooterViewHolder){
+            FooterViewHolder footerViewHolder = (FooterViewHolder) holder;
+
+        }else{
+            ItemViewHolder itemViewHolder = (ItemViewHolder) holder;
+            bodyBind(itemViewHolder, position);
+        }
+    }
+    private void bodyBind(final ItemViewHolder holder, final int position){
+
+        Data abc = listData.get(position - 1);
         holder.textView1.setText(abc.getTitle());
         holder.textView2.setText(abc.getContent());
         holder.textView3.setText(abc.getPrice());
         holder.imageView.setImageResource(abc.getResId());
     }
-
     @Override
     public int getItemCount() {
         // RecyclerView의 총 개수 입니다.
-        return listData.size();
-    }
+        return listData.size()+2;}
+
     // RecyclerView의 핵심인 ViewHolder 입니다.
     class ItemViewHolder extends RecyclerView.ViewHolder {
         public TextView textView1;
@@ -75,9 +109,9 @@ public class CafeAdapter extends RecyclerView.Adapter<CafeAdapter.ItemViewHolder
                 public void onClick(View view) {
                     int pos = getAdapterPosition();
                     if(pos != RecyclerView.NO_POSITION){
-                            if(CafeListener != null){
-                                CafeListener.onItemClick(view, pos);
-                            }
+                        if(CafeListener != null){
+                            CafeListener.onItemClick(view, pos);
+                        }
                     }
                 }
             });
@@ -87,6 +121,14 @@ public class CafeAdapter extends RecyclerView.Adapter<CafeAdapter.ItemViewHolder
         listData = filteredList;
         notifyDataSetChanged();
     }
+    class FooterViewHolder extends RecyclerView.ViewHolder{
+        FooterViewHolder(View footerView){
+            super(footerView);
+        }
+    }
+    class HeaderViewHolder extends RecyclerView.ViewHolder{
+        HeaderViewHolder(View headerView){
+            super(headerView);
+        }
+    }
 }
-
-
--- app/src/main/java/com/example/cafe/MainActivity.java
+++ app/src/main/java/com/example/cafe/MainActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,85 +21,125 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-     private CafeAdapter adapter;
-     private  ArrayList<Data> listData;
-     RecyclerView recyclerView;
-     EditText editText;
-      @Override
-     protected void onCreate(Bundle savedInstanceState) {
+    private CafeAdapter adapter;
+    private ArrayList<Data> listData;
+    RecyclerView recyclerView;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         getData();
         init();
         setSearch();
+        getOrder();
         MenuOptionClickEvent();
-
-
     }
-     private void init(){
 
-
+    private void init() {
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
+
         LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(layoutManager );
+        recyclerView.setLayoutManager(layoutManager);
 
         adapter = new CafeAdapter(listData);
         recyclerView.setAdapter(adapter);
 
     }
-     private void getData() {
+
+    private void getData() {
         listData = new ArrayList<>();
         // 임의의 데이터입니다.
-        listData.add(new Data( "핸드드립", "핸드드립입니다.", "1원", R.drawable.a));
+        listData.add(new Data("핸드드립", "핸드드립입니다.", "1원", R.drawable.a));
         listData.add(new Data("아메리카노", "아메리카노입니다.", "2원", R.drawable.b));
         listData.add(new Data("에스프레소", "에스프레소입니다", "3원", R.drawable.c));
         listData.add(new Data("카페모카", "카페모카입니다", "4원", R.drawable.d));
         listData.add(new Data("카푸치노", "카푸치노입니다", "5원", R.drawable.e));
         listData.add(new Data("카라멜 마끼아또", "카라멜 마끼아또입니다", "6원", R.drawable.f));
-        listData.add(new Data("그린티라떼", "그린티라떼입니다.","7원",R.drawable.g));
-        listData.add(new Data("초코라떼","초코라떼입니다","8원", R.drawable.h));
+        listData.add(new Data("그린티라떼", "그린티라떼입니다.", "7원", R.drawable.g));
+        listData.add(new Data("초코라떼", "초코라떼입니다", "8원", R.drawable.h));
         listData.add(new Data("카페라떼", "카페라떼입니다", "9원", R.drawable.i));
         listData.add(new Data("콜드브루", "콜드브루입니다", "10원", R.drawable.j));
         listData.add(new Data("밀크티", "밀크티입니다", "11원", R.drawable.k));
         listData.add(new Data("레몬티", "레몬티입니다", "12원", R.drawable.l));
         listData.add(new Data("유자차", "유자차입니다", "13원", R.drawable.m));
-        listData.add(new Data("얼그레이티", "얼그레이티입니다","14원", R.drawable.n));
+        listData.add(new Data("얼그레이티", "얼그레이티입니다", "14원", R.drawable.n));
         listData.add(new Data("바닐라라떼", "바닐라라떼입니다", "15원", R.drawable.o));
-        listData.add(new Data("123,","456","16원",R.drawable.p));
-        Log.v("여기까진됨22","메시지");
-        }
+        listData.add(new Data("123,", "456", "16원", R.drawable.p));
+    }
 
-     private void filter(String text){
-            ArrayList<Data> filteredList = new ArrayList<>();
-            for (Data SearchData : listData){
-                if(SearchData.getTitle().toLowerCase().contains(text.toLowerCase())){
-                    filteredList.add(SearchData);
-                        }
+    private void filter(String text) {
+        ArrayList<Data> filteredList = new ArrayList<>();
+        for (Data SearchData : listData) {
+            if (SearchData.getTitle().toLowerCase().contains(text.toLowerCase())) {
+                filteredList.add(SearchData);
+            }
+        }
+        adapter.filterList(filteredList);
+    }
+
+    public void setSearch() {
+        editText = (EditText) findViewById(R.id.menu_search);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                filter(editable.toString());
+            }
+        });
+    }
+    public void getOrder(){
+
+        final GestureDetector gestureDetector = new GestureDetector(MainActivity.this,new GestureDetector.SimpleOnGestureListener()
+        {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e)
+            {
+                return true;
+            }
+        });
+        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+                Log.d("TAG","onInterceptTouchEvent");
+                View child = rv.findChildViewUnder(e.getX(), e.getY());
+                if(child!=null && gestureDetector.onTouchEvent(e)) {
+                    TextView price = (TextView) rv.getChildViewHolder(child).itemView.findViewById(R.id.textView3);
+                    TextView TotalPrice = (TextView)findViewById(R.id.TextView5);
+                    Toast.makeText(getApplication(), price.getText().toString(), Toast.LENGTH_SHORT).show();
                 }
-            adapter.filterList(filteredList);
-      }
-     public void setSearch(){
-       editText= (EditText)findViewById(R.id.menu_search);
-       editText.addTextChangedListener(new TextWatcher() {
-           @Override
-           public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
-           @Override
-           public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
-           @Override
-           public void afterTextChanged(Editable editable) {
-               filter(editable.toString());
-           }
-       });
-     }
-     public void MenuOptionClickEvent(){
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+
+
+    }
+    public void MenuOptionClickEvent(){
         adapter.setOnItemClickListener(new CafeAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(View view, int pos) {
                 final ArrayList<Object> selectedItems = new ArrayList<>();
-
-                new AlertDialog.Builder(MainActivity.this)
+                new androidx.appcompat.app.AlertDialog.Builder(MainActivity.this)
                         .setTitle("추가메뉴")
                         .setMultiChoiceItems(R.array.menu, null, new DialogInterface.OnMultiChoiceClickListener() {
                             @Override
@@ -110,7 +153,6 @@ public class MainActivity extends AppCompatActivity {
                                 }
                             }
                         })
-
                         .setPositiveButton("확인", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
@@ -118,7 +160,7 @@ public class MainActivity extends AppCompatActivity {
                                 String[] options = getResources().getStringArray(R.array.menu);
                                 String string = "선택하신 추가 옵션은 ";
                                 for (int i = 0; i < selectedItems.size(); i++) {
-                                    string += options[i] + "  ";
+                                    string += options[i] + " ";
 
                                 }
                                 Toast.makeText(MainActivity.this, string +" 입니다.", Toast.LENGTH_SHORT).show();
@@ -127,6 +169,13 @@ public class MainActivity extends AppCompatActivity {
                         })
                         .setNegativeButton("취소", null)
                         .show();
+            }
+        });
+    }
+    public void TotalPrice(){adapter.setOnItemClickListener(new CafeAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int pos) {
+
             }
         });
     }
